46e3ba7c26eee0a4ddea903c2557ee43ff9cc0a6
pavel.avsenin <pavel.avsenin@fivetran.com>
2022-06-07 10:38:56 +0200

    Add
    Add

--- models/sheets_model.sql
+++ models/sheets_model.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     birthday as first_order_date
 
-from `dbt-technical-training`.dbt_pavsenin.sheet_table
+from `dbt-technical-training`.dbt_pavsenin.sheet_table_staging
