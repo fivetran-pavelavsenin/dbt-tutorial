318cd7c047ee78fd4a645f7d13ca404451599a19
pavel.avsenin <pavel.avsenin@fivetran.com>
2022-06-20 17:16:40 +0200

    Fix
    Fix

--- models/sheets_model.sql
+++ models/sheets_model.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     birthday as first_order_date
 
-from `dbt-technical-training`.dbt_pavsenin.sheet_table_staging
+from  {{ source('dbt_pavsenin', 'sheet_table_staging') }}
--- models/sheets_model2.sql
+++ models/sheets_model2.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     first_name as first_name
 
-from `dbt-technical-training`.dbt_pavsenin.sheet_table_staging
+from  {{ source('dbt_pavsenin', 'sheet_table_staging') }}
